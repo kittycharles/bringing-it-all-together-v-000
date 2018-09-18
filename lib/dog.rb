f60dbd57aa564578e9cb32cc24535f495c811418
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 17:11:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,8 @@
 class Dog
-  attr_accessor :id, :name, :breed
+  attr_accessor :name, :breed
+  attr_reader :id
 
-  def initialize(name, breed, id = nil)
+  def initialize(id = nil, name, breed)
     @name = name
     @breed = breed
     @id = id
